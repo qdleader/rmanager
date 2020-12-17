48ad92f4aeeef12122993880d544198547211b69
qdleader <yk4545945@163.com>
2020-12-17 14:08:05 +0800

    添加btn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
+
+import {Button} from 'antd'
+import 'antd/dist/antd.css'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Button type="primary">按钮</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
